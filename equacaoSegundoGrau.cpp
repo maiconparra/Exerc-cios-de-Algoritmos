9b26ee3256f3cf7d7972c29233568c8f18f8fcaf
maiconparra <maiconparradasilva@gmail.com>
2019-10-18 20:31:37 -0300

    Iniciando APPs

---
+++ equacaoSegundoGrau.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class equcaoSegundoGrau{
+      
+      private:
+              int a, b, c;
+              int delta, v1, v2;    
+      };
+
+
+
+int main(){
+ 
+ 
+ return 0;   
+}
